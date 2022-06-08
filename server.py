6b05a59a13fbc65e44651983fc479e3ddaf61b46
Noelle Notermann <noona020@umn.edu>
2022-06-07 23:07:55 -0500

    works to call api; hardcoded product ID

--- server.py
+++ server.py
@@ -1,8 +1,17 @@
 """Server for myRetail RESTful API"""
 
 from flask import Flask
+import urllib.request
+from urllib.request import urlopen
+import json
+
+import os
+
 
 app = Flask(__name__)
+
+
+API_KEY = os.environ['REDSKY_KEY']
 
 
 @app.route("/")
@@ -13,9 +22,14 @@ def welcome():
 def products_landing():
     return "<p>Getting warmer. Why not try a product ID?</p>"
 
-@app.route("/products/<id>")
-def redsky_call():
-    pass
+@app.route("/products/test")
+def get_redsky_info():
+    url = "https://redsky-uat.perf.target.com/redsky_aggregations/v1/redsky/case_study_v1?key={}&tcin=84836363".format(API_KEY)
+
+    response = urllib.request.urlopen(url)
+    data = response.read()
+
+    return data
 
 
 if __name__ == '__main__':
